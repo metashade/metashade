d2056fee254c1931a9866e2019c11b88101e4f45
Pavlo Penenko <pavlo.penenko@gmail.com>
2026-01-18 12:46:41 -0500

    Update test refs

--- tests/ref/test_rgba_rgb_augmented.glsl
+++ tests/ref/test_rgba_rgb_augmented.glsl
@@ -2,7 +2,6 @@
 vec4 rgba_rgb_augmented(vec4 rgba, vec3 rgb)
 {
 	rgba.rgb += rgb;
-	rgba.rgb = rgba.rgb;
 	return rgba;
 }
 
--- tests/ref/test_rgba_rgb_augmented.hlsl
+++ tests/ref/test_rgba_rgb_augmented.hlsl
@@ -1,7 +1,6 @@
 float4 rgba_rgb_augmented(float4 rgba, float3 rgb)
 {
 	rgba.rgb += rgb;
-	rgba.rgb = rgba.rgb;
 	return rgba;
 }
 
