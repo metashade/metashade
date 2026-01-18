4ca6469f009df679e57c11c13d0cc1fe7ded1817
Pavlo Penenko <pavlo.penenko@gmail.com>
2026-01-18 11:28:12 -0500

    Update test references

--- tests/ref/test_augmented_swizzle.glsl
+++ tests/ref/test_augmented_swizzle.glsl
@@ -2,7 +2,9 @@
 vec2 test_swizzle(vec2 v, float f)
 {
 	v.x += f;
+	v.x = v.x;
 	v.xy += v;
+	v.xy = v.xy;
 	return v;
 }
 
--- tests/ref/test_augmented_swizzle.hlsl
+++ tests/ref/test_augmented_swizzle.hlsl
@@ -1,7 +1,9 @@
 float2 test_swizzle(float2 v, float f)
 {
 	v.x += f;
+	v.x = v.x;
 	v.xy += v;
+	v.xy = v.xy;
 	return v;
 }
 
