dc510322f90a90e0c6f71cf243ff0febdbda0f93
Pavlo Penenko <pavlo.penenko@gmail.com>
2026-01-18 11:36:13 -0500

    test: Add test for sh.a += sh.a (self-augmented without _ syntax)

---
+++ tests/ref/test_augmented_self_add.glsl
@@ -0,0 +1,11 @@
+#version 450
+vec4 self_add(vec4 a)
+{
+	a += a;
+	return a;
+}
+
+void main()
+{
+}
+
---
+++ tests/ref/test_augmented_self_add.hlsl
@@ -0,0 +1,10 @@
+float4 self_add(float4 a)
+{
+	a += a;
+	return a;
+}
+
+void main()
+{
+}
+
